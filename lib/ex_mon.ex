1d98696260f103b228acffa98a80b8fcaf26bc4a
José Ferreira <dev.josejunior@gmail.com>
2023-03-23 00:48:22 -0300

    feat: create game agent

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,12 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
+  end
+
+  def start_game(player) do
+    computer = create_player("Computer", :punch, :kick, :heal)
+    Game.start(computer, player)
   end
 end
