5b6cee7aba6169aa2b60577252b972c80faeee75
José Ferreira <dev.josejunior@gmail.com>
2023-03-23 00:29:41 -0300

    create player module constants

--- lib/player.ex
+++ lib/player.ex
@@ -1,10 +1,14 @@
 defmodule ExMon.Player do
-  @enforce_keys [:life, :name, :move_avg, :move_rnd, :move_heal]
-  defstruct [:life, :name, :move_avg, :move_rnd, :move_heal]
+  @required_keys [:life, :name, :move_avg, :move_rnd, :move_heal]
+  @max_life 100
+
+  @enforce_keys @required_keys
+  defstruct @required_keys
+
 
   def build(name, move_rnd, move_avg, move_heal) do
     %ExMon.Player{
-      life: 100,
+      life: @max_life,
       name: name,
       move_avg: move_avg,
       move_rnd: move_rnd,
