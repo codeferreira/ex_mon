6f42f14243fd73cb2195f90c386c3de0c705d79a
José Ferreira <dev.josejunior@gmail.com>
2023-03-29 01:11:50 -0300

    feat: add printing attack status

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -12,4 +12,16 @@ defmodule ExMon.Game.Status do
     IO.puts("Move: #{move}")
     IO.puts("----------------------------")
   end
+
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\n====== Player attacked Robotnik dealing #{damage} damage ======\n")
+  end
+
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("\n====== Robotnik attacked player dealing #{damage} damage ======\n")
+  end
+
+  def print_move_message(:player, :heal, damage) do
+    IO.puts("\n====== Player healed #{damage} life ======\n")
+  end
 end
